3dc7642ea61891c090e5bb9028bca5f1ea3c44e8
hog099 <oliveiraginelli@gmail.com>
2020-07-28 11:34:42 -0300

    first commit"

---
+++ src/pages/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="content">
+        <h4 className="texttitle">PWA Starter.</h4>
+        <p className="textsubtitle">Reactjs web base para criação de PWA, se atentar no serviceWorker register para cahce da aplicação e performance, assim sendo todas demais featrues podem aplicados como estrtura que costuma trabalhar, claro que se atentando na responsividade.</p>
+    </div>
+  );
+}
+
+export default App;
